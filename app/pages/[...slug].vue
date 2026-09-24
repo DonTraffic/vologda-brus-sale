c0c2634b29e2dc48895e730b4f7891169b44a914
KaeruYami <dogerfantom6@gmail.com>
2026-09-24 19:15:29 +0300

    Старт проекта (исправление)

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-const route = useRoute()
-
-const { data: page } = await useAsyncData('page-' + route.path, () => {
-  return queryCollection('content').path(route.path).first()
-})
-
-if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
-}
+const VK_GROUP_URL = 'https://vk.ru/vologdabrus'
 </script>
 
 <template>
-  <ContentRenderer
-    v-if="page"
-    :value="page"
-  />
+  <p>
+    Переходим в группу ВКонтакте…
+    <a :href="VK_GROUP_URL">{{ VK_GROUP_URL }}</a>
+  </p>
 </template>
